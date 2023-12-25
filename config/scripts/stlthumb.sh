216a9fe7a4f8cee0aca7f33c259ca37f778dff30
alextrical <35117191+alextrical@users.noreply.github.com>
2023-12-25 20:29:01 +0000

    STL Thumb release download fix

--- config/scripts/stlthumb.sh
+++ config/scripts/stlthumb.sh
@@ -9,6 +9,6 @@ set -oue pipefail
 wget https://github.com/alextrical/stl-thumb/releases/download/release/stl-thumb-0.5.0-1.x86_64.rpm -P /tmp
 rpm-ostree install /tmp/stl-thumb-0.5.0-1.x86_64.rpm
 rm /tmp/stl-thumb-0.5.0-1.x86_64.rpm
-wget https://github.com/alextrical/stl-thumb-kde/releases/download/Release/stlthumbnail.so -P /usr/lib64/qt5/plugins
+wget https://github.com/alextrical/stl-thumb-kde/releases/download/release/stlthumbnail.so -P /usr/lib64/qt5/plugins
 chmod +x /usr/lib64/qt5/plugins/stlthumbnail.so
-wget https://github.com/alextrical/stl-thumb-kde/releases/download/Release/stlthumbnail.desktop -P /usr/share/kservices5
+wget https://github.com/alextrical/stl-thumb-kde/releases/download/release/stlthumbnail.desktop -P /usr/share/kservices5
